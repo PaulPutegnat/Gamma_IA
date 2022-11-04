fb68f0e3d8db6515392af494bb936c09c5e7c473
Paul Putegnat <paul.putegnat@edu.devinci.fr>
2022-11-04 09:52:41 +0100

    finished broken Shockwave

--- Assets/Teams/Gamma/Scripts/Shockwave.cs
+++ Assets/Teams/Gamma/Scripts/Shockwave.cs
@@ -7,8 +7,20 @@ namespace GammaTeam
 	[TaskCategory("IIM")]
 	public class Shockwave : Action
 	{
+		GameData gData = GameManager.Instance.GetGameData();
+		SpaceShipView spaceship;
+		SpaceShip ship;
+
 		public TaskStatus CastShockwave()
 		{
+			spaceship = gData.GetSpaceShipForOwner(spaceship.Owner);
+			if (gData.GetSpaceShipForOwner(1 - spaceship.Owner).Orientation != spaceship.Orientation)
+            {
+				if(gData.GetSpaceShipForOwner(1 - spaceship.Owner).Position.x == spaceship.Position.x - 100 || gData.GetSpaceShipForOwner(1 - spaceship.Owner).Position.y == spaceship.Position.y)
+                {
+					ship.FireShockwave();
+                }
+            }
 			return TaskStatus.Success;
 		}
 	}
